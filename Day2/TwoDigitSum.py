c3505db352a9c69dc934704c27f9ac6e08f2c29f
Nagadath Gummadi <51068943+Nagadath-Gummadi@users.noreply.github.com>
2021-04-09 23:09:50 +0530

    Add files via upload

---
+++ Day2/TwoDigitSum.py
@@ -0,0 +1,37 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+print(int(two_digit_number[0])+int(two_digit_number[1]))
+
+
+
+
+
+
+"""
+Instructions
+Write a program that adds the digits in a 2 digit number. e.g. if the input was 35, then the output should be 3 + 5 = 8
+
+
+# Example Input
+
+```
+39
+```
+# Example Output
+
+3 + 9 = 12
+
+```
+12
+```
+# Hint
+
+#1. Try to find out the data type of two_digit_number.
+#2. Think about what you learnt about subscripting.
+#3. Think about type conversion.
+"""
