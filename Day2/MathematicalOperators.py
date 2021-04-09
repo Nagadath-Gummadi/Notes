36dbb54a75120acc3773b3175e25af00d60e442e
Nagadath Gummadi <51068943+Nagadath-Gummadi@users.noreply.github.com>
2021-04-09 23:07:10 +0530

    Add files via upload

---
+++ Day2/MathematicalOperators.py
@@ -0,0 +1,9 @@
+print('Operand 1:3, Operand 2:2')
+print('Addition 3+2: ',3+2) 	#Addition
+print('Subtraction 3-2: ',3-2)	#Subtraction
+print('Multiplication 3*2: ',3*2)	#Multiplication
+print('Division ->Gives the floating value (quotient) 3/2: ',3/2)	#Division ->Gives the floating value (quotient)
+print('Modular Division ->Gives the remainder 3%2:',3%2)	#Modular Division ->Gives the remainder
+print('Exponent 3**2: ',3**2)	#Exponent
+print('Floor Division ->floor(3/2) 3//2',3//2)	#Floor Division ->floor(3/2)
+
