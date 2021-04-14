7b98d2f47fd546a85a88e711f8dcdd2b9e8c5d97
Nagadath Gummadi <51068943+Nagadath-Gummadi@users.noreply.github.com>
2021-04-14 22:42:55 +0530

    Add files via upload

---
+++ Day3/RollerCoaster_Eligibility.py
@@ -0,0 +1,6 @@
+print('Welcome to roller coaster')
+height=int(input('Enter your height in cm:'))
+if height>=160:
+    print("Hey! you are eligible to this ride. Enjoy the day")
+else:
+    print("Oops! You are Not Eligible with this height.")
