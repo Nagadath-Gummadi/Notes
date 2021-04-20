bb45ef6b80ea8e2d7ffc5d3c9fdb1a26c23106aa
Nagadath Gummadi <51068943+Nagadath-Gummadi@users.noreply.github.com>
2021-04-20 13:45:47 +0530

    Add files via upload

---
+++ Day4/ListOperations-2.py
@@ -0,0 +1,11 @@
+lst=[3,1,6,8,4,2,7,9,6,3]
+print(lst)
+print(sorted(lst)) #returns the sorted order
+print(lst)
+lst.sort() #sorts the list and returns None
+print(lst)
+print(lst.count(6)) #returns the number of occurances of the given element in the list
+lst.reverse() #reverses the list and returns None
+print(lst)
+lst.clear() #clears the list and returns None
+print(lst)
