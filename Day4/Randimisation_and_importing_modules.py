be3470e1db352d03fc9828f377f01b6c45f8e69c
Nagadath Gummadi <51068943+Nagadath-Gummadi@users.noreply.github.com>
2021-04-20 13:46:33 +0530

    Add files via upload

---
+++ Day4/Randimisation_and_importing_modules.py
@@ -0,0 +1,9 @@
+import random
+import my_module as mm
+a=random.randint(1,10) #generates a random int between (a-b) both a,b are inclusive
+b=random.random() #generates a random fload (0-1) 0-inclusive,1-exclusive
+print(a)
+print(b)
+pi=3
+print(mm.pi)
+print(pi)
