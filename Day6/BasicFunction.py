61e073d6f0c614e5915c1d10f7caf4cb8563cfde
Nagadath-Gummadi <nagadath28@gmail.com>
2023-10-21 21:05:04 -0300

    Small change. Playing with git commands

--- Day6/BasicFunction.py
+++ Day6/BasicFunction.py
@@ -1,4 +1,4 @@
-#Avoids code repitiion
+#Functions : Reusable, Avoids code repitiion
 #Function is a block of code which only runs when it is called
 #You can pass data, known as parameters, into a function
 
